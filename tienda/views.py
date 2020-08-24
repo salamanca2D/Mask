185dd9de72aebf91b80bd018ed1b66a23a959d65
SALAMANCA <salamanca.int@gmail.com>
2020-08-23 20:07:17 -0600

    funtional

--- tienda/views.py
+++ tienda/views.py
@@ -1,15 +1,109 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
 
+from .models import *
 # Create your views here.
 
 def tienda(request):
-     context = {}
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
+     else:
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+          cartItems = order['get_cart_items']
+
+     productos = Product.objects.all()
+     context = {'productos':productos, 'cartItems':cartItems}
      return render(request, 'tienda/tienda.html', context)
 
 def cart(request):
-     context = {}
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
+     else:
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+          cartItems = order['get_cart_items']
+
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
      return render(request, 'tienda/carro.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'tienda/checkout.html', context)
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          items = order.orderitem_set.all()
+          cartItems = order.get_cart_items
+     else:
+          items = []
+          order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+          cartItems = order['get_cart_items']
+
+     context = {'items':items, 'order':order, 'cartItems':cartItems}
+     return render(request, 'tienda/checkout.html', context)
+
+def updateItem(request):
+     data = json.loads(request.body)
+     productId = data['productId']
+     action = data['action']
+
+     print('action', action)
+     print('productId', productId)
+
+     customer = request.user.customer
+     product = Product.objects.get(id=productId)
+     order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+     if action == 'add':
+          orderItem.quantity = (orderItem.quantity + 1)
+     elif action == 'remove':
+          orderItem.quantity = (orderItem.quantity - 1)
+
+     orderItem.save()
+
+     if orderItem.quantity <= 0:
+          orderItem.delete()
+
+     return JsonResponse('Item was added', safe=False)
+
+#from django.views.decorators.csrf import csrf_exempt
+
+#@csrf_exempt
+def processOrder(request):
+     transaction_id = datetime.datetime.now().timestamp()
+     data = json.loads(request.body)
+
+     if request.user.is_authenticated:
+          customer = request.user.customer
+          order, created = Order.objects.get_or_create(customer=customer, complete=False)
+          total = float(data['form']['total'])
+          order.transaction_id = transaction_id
+
+          if total == order.get_cart_total:
+               order.complete = True
+          order.save()
+
+          if order.shipping == True:
+               ShippingAddress.objects.create(
+                    customer=customer,
+                    order=order,
+                    address=data['shipping']['address'],
+                    city=data['shipping']['city'],
+                    state=data['shipping']['state'],
+                    zipcode=data['shipping']['zipcode'],
+               )
+          
+     else:
+          print('usuario no esta loginado')
+     return JsonResponse('Pago completo', safe=False)
